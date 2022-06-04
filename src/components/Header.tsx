ae6a72e6eb9c83e2fbecf7b800a6fc6c5f1f9bc9
Jessé <jesseoito@gmail.com>
2022-06-04 13:56:08 -0300

    Adding links to login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
+
+import styles from "../styles/Header.module.scss";
 
 export default function Header() {
-    return (
-        <h2>
-          <Link href="/">
-            <a className="hover:underline">Matching-bet</a>
-          </Link>
-        </h2>
-      )
+  return (
+    <header className={styles.header}>
+      <h2>
+        <a className="hover:underline">Matching-bet</a>
+      </h2>
+
+      <div className={styles.links}>
+        <Link href="/login">
+          <a className="hover:underline">Login</a>
+        </Link>
+
+        <Link href="/registro">
+          <a className="hover:underline">Registro</a>
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
-import Header from '../components/Header';
+import Header from '../components/header';
 
 const Home: NextPage = () => {
   return (
