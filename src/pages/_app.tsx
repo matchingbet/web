30d22735e7b818339aef831142915740cf345130
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-06-04 16:04:14 -0300

    feat: add default mui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
-import Header from '../components/header';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
