ec62e832354377bda092277628be0bbc3a3fa041
Jessé Franklin <jesseoito@gmail.com>
2022-12-11 04:10:27 -0300

    adding refresh token to test

--- src/core/http-client-adapter.ts
+++ src/core/http-client-adapter.ts
@@ -1,23 +1,22 @@
-import useSecurityStore from "../stores/SecurityStore";
 
 export class HttpClient {
 
     private _baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
-    get(endpoint: string, id?: number, options?: any): Promise<Response> {
+    async get(endpoint: string, id?: number, options?: any): Promise<Response> {
         let url = `${this._baseUrl}/${endpoint}`;
         if (id) {
             console.log(url);
             url = `${url}/${id}`;
         }
-        
+
         return fetch(url, {
             method: "GET",
             headers: this._buildHeaders(options)
         });
     }
 
-    post(endpoint: string, body: any, options?: any): Promise<Response> {
+    async post(endpoint: string, body: any, options?: any): Promise<Response> {
 
         const headers = this._buildHeaders(options)
 
@@ -30,7 +29,7 @@ export class HttpClient {
         });
     }
 
-    getById(endpoint: string, id?: number, options?: any): Promise<Response> {
+    async getById(endpoint: string, id?: number, options?: any): Promise<Response> {
         let url = `${this._baseUrl}/${endpoint}`;
         if (id) {
             url = `${url}/${id}`;
@@ -41,7 +40,7 @@ export class HttpClient {
         });
     }
 
-    update(endpoint: string, body: any, id?: number, options?: any): Promise<Response> {
+    async update(endpoint: string, body: any, id?: number, options?: any): Promise<Response> {
         let url = `${this._baseUrl}/${endpoint}`;
         if (id) {
             url = `${url}/${id}`;
@@ -54,16 +53,41 @@ export class HttpClient {
     }
 
     private _buildHeaders(options?: any) {
+
+        this._refreshToken();
+
         const authStorageString = window.localStorage.getItem("auth_store");
         const token = !!authStorageString ? JSON.parse(authStorageString).state.token : null;
 
         return {
             ...options,
-            ...token ? {"Authorization": `bearer ${token}`} : {},
+            ...token ? { "Authorization": `bearer ${token}` } : {},
             "Accept": "application/json",
             "Content-Type": options && options["Content-Type"] ? options["Content-Type"] : "application/json"
         };
     }
+
+    async _refreshToken() {
+        const authStorageString = window.localStorage.getItem("auth_store");
+        const refreshToken = !!authStorageString ? JSON.parse(authStorageString).state.refresh_token : null;
+
+        const basicToken = process.env.NEXT_PUBLIC_BASIC_API_TOKEN;
+        const options = { "Authorization": `Basic ${basicToken}`, "Content-Type": "application/x-www-form-urlencoded" }
+        const encodedToken = { ...refreshToken, "grant_type": "refresh_token" };
+        const response = await this.post("/oauth/token", encodedToken, options);
+
+        if (response.ok) {
+            const body = await response.json();
+            const authToken = body["access_token"];
+            console.log(body["users_id"]);
+            const authStorageString = window.localStorage.getItem("auth_store");
+            const authStorageStringState = !!authStorageString ? JSON.parse(authStorageString).state : null;
+            authStorageStringState['token'] = authToken;
+            window.localStorage.setItem("auth_store", JSON.stringify(authStorageStringState));
+        }
+
+    }
+
 
 }
 
